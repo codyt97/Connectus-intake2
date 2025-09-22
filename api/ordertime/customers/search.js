74817a773c4bfe752d50a847c0331f7406fde043
codyt97 <87399610+codyt97@users.noreply.github.com>
2025-09-22 19:40:27 -0400

    Update search.js

--- api/ordertime/customers/search.js
+++ api/ordertime/customers/search.js
@@ -7,7 +7,6 @@ export default async function handler(req, res) {
     if (!q0) return res.status(200).json([]);
 
     const q = q0.replace(/\s+/g, ' ').trim();
-
     const like = (prop) => ({
       PropertyName: prop,
       FieldType: 1,      // string
@@ -17,14 +16,14 @@ export default async function handler(req, res) {
 
     const calls = [
       otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('Name')] }),
-      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('CompanyName')] }),
-      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('ShipToCompany')] }).catch(() => []),
-      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('ShipToCity')] }).catch(() => []),
-      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('ShipToState')] }).catch(() => []),
-      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('ShipToZip')] }).catch(() => []),
-      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('BillingCity')] }).catch(() => []),
-      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('BillingState')] }).catch(() => []),
-      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('BillingZip')] }).catch(() => []),
+      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('CompanyName')] }).catch(()=>[]),
+      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('ShipToCompany')] }).catch(()=>[]),
+      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('ShipToCity')] }).catch(()=>[]),
+      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('ShipToState')] }).catch(()=>[]),
+      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('ShipToZip')] }).catch(()=>[]),
+      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('BillingCity')] }).catch(()=>[]),
+      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('BillingState')] }).catch(()=>[]),
+      otPost('/list', { Type: 120, NumberOfRecords: 25, PageNumber: 1, Filters: [like('BillingZip')] }).catch(()=>[]),
     ];
 
     const results = await Promise.all(calls);
